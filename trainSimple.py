74aaec8b294186c181127da579f98c8c4675d139
qvuer7 <qvuer7@gmail.com>
2023-04-30 15:27:14 +0100

    train simple BCE changed to CE

--- trainSimple.py
+++ trainSimple.py
@@ -80,7 +80,7 @@ def evaluate(model, criterion, dataloader, device, epoch, save_path):
 def criterion(inputs, target):
     losses = {}
     for name, x in inputs.items():
-        losses[name] = nn.functional.binary_cross_entropy(x, target)
+        losses[name] = nn.functional.cross_entropy(x, target, ignore_index = 255)
 
 
     if len(losses) == 1:
